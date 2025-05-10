3be75f3c274c4520dc2aa8a569c65270da4ff6d1
Akshat Bansal <akshatbansal008@gmail.com>
2025-05-10 10:22:57 +0530

    updated the logic

--- Revising Python/03_life_advice.py
+++ Revising Python/03_life_advice.py
@@ -1,5 +1,10 @@
-from Lemon import lemon
+# from Lemon import lemon
+
+# print('When life gives you lemons')
+# answer= lemon.lemonade()
+# print(answer)
+
+from Lemon.lemon import lemonade
 
 print('When life gives you lemons')
-answer= lemon.lemonade()
-print(answer)
+print(lemonade())
