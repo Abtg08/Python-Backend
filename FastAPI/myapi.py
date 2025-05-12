5c5898389fa0a349aaa8f8b28135e183815f94c9
Akshat Bansal <akshatbansal008@gmail.com>
2025-05-13 02:18:19 +0530

    api creation learning

--- FastAPI/myapi.py
+++ FastAPI/myapi.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI, Path
+
 app = FastAPI()
 
-students= {
+students = {
     1: {
         "name": "John",
         "age": 20,
         "class": 'Year 12'
-    }
+    },
+    # 3: {                              this shows that the value being passed is a dictionary and is not case sensitive
+    #     'name': 'Anya',
+    #     'age': 19,
+    #     'role': 'Hero'    
+    # }
 }
 
 @app.get('/')
@@ -14,5 +20,5 @@ def index():
     return {"name": "First Data"}
 
 @app.get('/get-student/{student_id}')
-def get_student(student_id:int = Path(None, description="The ID of the student you want to view")):
-  return students[student_id]
+def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt=0, lt=5)):
+    return students[student_id]
